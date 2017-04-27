ea0b9a834fcb1570b5bc21e557f83e5f27f306db
DESKTOP-313R62C\Louis <louis.groisne@gmail.com>
2017-04-27 19:18:03 +0200

    Work in progress for the following car feature.

---
+++ Assets/Scripts/FollowingCar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FollowingCar : MonoBehaviour {
+
+	//The goal of this script
+	//Is to get the nearest target
+	//Get to it
+	//Destroy it
+	//Go to next Target
+
+	private GameObject currentTarget;
+	private Rigidbody rb;
+
+	public List<GameObject> targets;
+	public float speed;
+
+	void Start () {
+		targets = new List<GameObject>();
+		rb = GetComponent<Rigidbody>();
+		if (speed == 0f){
+			speed = 10f;
+		}
+	}
+	void Update () {
+		if (currentTarget != null){
+			// Rotate to look at the target
+			transform.LookAt(currentTarget.transform.position);
+			// Translate in the direction of the target
+			transform.Translate(currentTarget.transform.position * Time.deltaTime * speed);
+			if ( Vector3.Distance(transform.position, currentTarget.transform.position) < Mathf.Epsilon * 10000000000f){
+				targets.Remove(currentTarget);
+				Destroy(currentTarget);
+				currentTarget = targets.FirstOrDefault();
+			}
+		} else {
+			currentTarget = targets.FirstOrDefault();
+		}
+	}
+}
---
+++ Assets/Scripts/StressEventSystem/Events/StreeEvent_FollowingCar.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreeEvent_FollowingCar : StressEvent_Immediate {
+
+	public Rigidbody carRigidbody;
+	public GameObject target;
+	public GameObject followingCarPrefab;
+	public float intervalTarget, spawnDistanceMultiplier;
+
+	private float currentDistance;
+	private bool isRunning;
+	private GameObject followingCar;
+	private FollowingCar followingCarScript;
+
+	// Use this for initialization
+	void Start () {
+		base.Start();
+		if (spawnDistanceMultiplier == 0f){
+			spawnDistanceMultiplier = 10f;
+		}
+		isRunning = false;
+	}
+
+
+	//------------------------------------------------------
+    //  Stress Event Start
+    //------------------------------------------------------
+    override public void StartEvent()
+    {
+        base.StartEvent();
+		isRunning = true;
+		currentDistance = 0f;
+		// Instantiate the car just behind the player
+		followingCar = Instantiate(followingCarPrefab, carRigidbody.transform.position - carRigidbody.transform.forward * spawnDistanceMultiplier, Quaternion.identity);
+		followingCarScript = followingCar.GetComponent<FollowingCar>();
+		// Debug.Log(followingCar);
+		// Debug.Log(followingCarScript);
+	}
+	//------------------------------------------------------
+	//  Stress Event End
+	//------------------------------------------------------
+	override public void EndEvent()
+    {
+        base.EndEvent();
+		isRunning = false;
+		Destroy(followingCar);
+    }
+
+	void FixedUpdate(){
+		if (isRunning){
+			//Distance since last frame depending on the speed of the car
+			currentDistance += carRigidbody.velocity.magnitude * Time.fixedDeltaTime;
+			if (currentDistance > intervalTarget){
+				followingCarScript.targets.Add(Instantiate(target, carRigidbody.transform.position, Quaternion.identity));
+				currentDistance = 0f;
+			}
+		}
+	}
+
+}
+
+
+// Role de l'event:
+// Instancier un objet "voiture qui suit"
+// Donner l'ordre à la voiture qui suit de dégager lorsque l'évènement se termine.
+// Faire en sorte que la voiture du joueur lache des targets à intervalle régulier.
+// Genre toutes les certaines distances.
