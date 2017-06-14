b49f9176ebec6a0d5e4b1ab733214ea0fbff8852
Benjamin <benjamin.roelandt@gmail.com>
2017-06-14 20:20:16 +0200

    Avance préliminaire pour le téléphone

---
+++ Assets/Scripts/PhoneState.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneState : MonoBehaviour {
+    public enum ePhoneState { Inactive, Ringing}
+
+    public List<Material> materials;
+
+    private MeshRenderer mr;
+    private AudioSource sound;
+
+    public bool Ringing = false; 
+    public bool CancelledCall = false;
+
+    // Use this for initialization
+    void Start () {
+        mr = GetComponent<MeshRenderer>();
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+		if(Ringing != sound.isPlaying)
+        {
+            if (Ringing)
+            {
+                sound.Play();
+               // mr.material = materials[1];
+            }
+            else
+            {
+                // mr.material = materials[0];
+                PhoneCallEnded();
+            }
+        }
+        if(CancelledCall)
+        {
+            sound.Stop();
+            Ringing = false;
+            CancelledCall = false;
+        }
+    }
+
+    public void CancelCall()
+    {
+        CancelledCall = true;
+    }
+
+    public void MakeRing()
+    {
+        Ringing = true;
+    }
+
+    public delegate void VoidEvent();
+    public event VoidEvent PhoneCallEnded;
+}
--- Assets/Scripts/StressEventSystem/Events/StressEvent_PhoneCall.cs
+++ Assets/Scripts/StressEventSystem/Events/StressEvent_PhoneCall.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StressEvent_PhoneCall : StressEvent_Immediate {
 
-	private bool isRinging;
-	public AudioSource audioSource;
 	public Text alertText;
+    public PhoneState phone;
 
 	//------------------------------------------------------
 	//  Stress Event Start
@@ -15,42 +15,35 @@ public class StressEvent_PhoneCall : StressEvent_Immediate {
 	override public void StartEvent()
     {
         base.StartEvent();
-		isRinging = true;
+        phone.MakeRing();
+        phone.PhoneCallEnded += CallEnded;
 		alertText.text = "Votre téléphone sonne. Prenez-le et rejetez l'appel.";
 	}
 
+    private void CallEnded()
+    {
+        Debug.Log("Le téléphone a fini de sonner");
+        EndEvent();
+    }
 
-	//------------------------------------------------------
-	//  Stress Event End
-	//------------------------------------------------------
-	override public void EndEvent()
+
+    //------------------------------------------------------
+    //  Stress Event End
+    //------------------------------------------------------
+    override public void EndEvent()
 	{
 		base.EndEvent();
 		alertText.text = "";
 	}
 
 	void Update() {
-		// Make the phone ring
-		if (isRinging && !audioSource.isPlaying){
-			Debug.Log("play");
-			audioSource.Play();
-
-		}
-		// Make the phone stop
-		if (!isRinging && audioSource.isPlaying){
-			Debug.Log("stop");
-			audioSource.Stop();
-		}
 
 
 		// Test the player's input to rejet the call
 		if (false){
-			RejectCall();
+			phone.CancelCall();
+            EndEvent();
 		}
 	}
 
-	public void RejectCall(){
-		isRinging = false;
-		// Call the end of the event
-	}
 }
