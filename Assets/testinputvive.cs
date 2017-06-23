40ad8acd0a839b908fd340de28d158c93eea78ac
Louis GROISNE <louis.groisne@gmail.com>
2017-06-23 16:22:28 +0200

    Work in progress for the ringing phone

---
+++ Assets/testinputvive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class testinputvive : MonoBehaviour {
+
+	SteamVR_Controller.Device device;
+	public SteamVR_TrackedObject controller;
+
+	Vector2 touchpad;
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		device = SteamVR_Controller.Input ((int)controller.index);
+		//If finger is on touchpad
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Touchpad)) {
+			//Read the touchpad values
+			touchpad = device.GetAxis (EVRButtonId.k_EButton_SteamVR_Touchpad);
+
+
+			Debug.Log (touchpad);
+		}
+	}
+}
